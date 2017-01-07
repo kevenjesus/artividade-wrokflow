82c0dd9e263b2746f9f6dd1de6b785e24faee1a9
KleberYokota <kleber_yokota@hotmail.com>
2017-01-06 23:28:02 -0200

    Atvidade 1 workflow

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp");
+var minifyHTML = require("gulp-minify-html-2");
+var options = {
+    comments:true, 
+    spare:true
+};
+var cleanCSS = require("gulp-clean-css");
+var optCSS = {
+	compatibility: 'ie8'
+};
+
+gulp.task('move-scss',function() {
+	return gulp.src('./source/scss/*.scss')
+		  .pipe(cleanCSS(optCSS))
+		  .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('minifyHTML',function() {
+	return gulp.src('./source/index.html')
+		   .pipe(minifyHTML(options))
+		   .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('background',function(){
+	gulp.watch('./source/index.html',['minifyHTML']);
+	gulp.watch('./source/scss/*.scss',['move-scss']);
+})
